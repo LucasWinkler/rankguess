559ac509a2d1bec4f8823092d1e733d202819cc3
Lucas Winkler <lucaswinkler@gmail.com>
2023-03-02 03:31:12 -0500

    Modified the background grid and the game cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import Head from 'next/head';
 import Container from '@/components/Container';
 
+const renderTempGrid = () => {
+  const items = [];
+
+  for (let i = 0; i < 8; i++) {
+    items.push(
+      <div
+        key={i}
+        className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-xl border border-blueish-grey-700/80 bg-blueish-grey-700 bg-opacity-[25%] p-6 text-center text-xl font-bold text-neutral-200 backdrop-blur-[1px] xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
+        Coming Soon
+      </div>
+    );
+  }
+
+  return items;
+};
+
 export default function Home() {
   return (
     <>
@@ -17,35 +33,12 @@ export default function Home() {
       <main id='main-content' className='relative py-12 lg:pt-16 lg:pb-32'>
         <Container>
           <div className='pointer-events-none absolute left-0 right-0 bottom-0 -top-[15.5rem] h-full w-full select-none bg-heading-circle bg-top bg-no-repeat'></div>
-          <div className='background-grid pointer-events-none absolute inset-0 select-none bg-bottom opacity-[5%]'></div>
+          <div className='background-grid pointer-events-none absolute inset-0 select-none bg-bottom opacity-[7.5%]'></div>
           <h1 className='relative text-center text-2xl font-medium uppercase tracking-[0.2em] text-neutral-200'>
             Choose a game
           </h1>
           <div className='grid-games relative mt-12 font-bold uppercase tracking-[0.2em] lg:mt-16'>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem] '>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
-            <div className='mx-auto flex min-h-[10rem] w-full max-w-[18rem] items-center justify-center rounded-lg border border-blueish-grey-700/50 bg-blueish-grey-700/50 text-center xs:max-w-[22rem] sm:min-h-[23.5rem] sm:max-w-[18rem]'>
-              Game Title
-            </div>
+            {renderTempGrid()}
           </div>
         </Container>
       </main>
