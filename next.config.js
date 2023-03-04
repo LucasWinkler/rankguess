76e88b52297aa8bd5015e12d52ab93ded09df0da
Lucas Winkler <lucaswinkler@gmail.com>
2023-03-03 19:58:54 -0500

    Removed swcMinify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     deviceSizes: [420, 768, 1024, 1280, 1536],
     imageSizes: [25, 33, 50, 100],
