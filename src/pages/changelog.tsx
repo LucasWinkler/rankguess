1c7687937fba59cc6b565720ab69d19c0df7df12
Lucas Winkler <lucaswinkler@gmail.com>
2023-03-06 01:18:04 -0500

    Adjusted changelog styling for mobile

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -10,19 +10,19 @@ export default function Changelog() {
           <div className='pointer-events-none absolute left-0 right-0 bottom-0 -top-[15.5rem] h-full w-full select-none bg-heading-circle bg-top bg-no-repeat'></div>
           <div className='background-grid pointer-events-none absolute inset-0 select-none opacity-[7.5%]'></div>
           <h1 className='page-heading-1 relative'>Changelog</h1>
-          <div className='relative rounded-xl border border-blueish-grey-600/80 bg-[#111B36] p-12'>
+          <div className='relative rounded-xl border border-blueish-grey-600/80 bg-[#111B36] p-8 sm:p-12'>
             {changelog.map(log => (
               <article key={log.version} className='[&:not(:first-child)]:mt-8'>
-                <h2 className='border-b border-b-blueish-grey-600/80 pb-3 text-2xl font-bold text-neutral-100'>
+                <h2 className='border-b border-b-blueish-grey-600/80 pb-3 text-xl font-bold text-neutral-100 sm:text-2xl'>
                   {log.version} ({log.date})
                 </h2>
                 <div>
                   {log.changes.map(item => (
                     <Fragment key={item.title}>
-                      <h3 className='mt-4 text-lg font-medium text-neutral-100'>
+                      <h3 className='mt-4 text-base font-medium text-neutral-100 sm:text-lg'>
                         {item.title}
                       </h3>
-                      <ul className='mt-2 list-inside list-disc'>
+                      <ul className='mt-2 list-inside list-disc text-sm sm:text-base'>
                         {item.change.map(change => (
                           <li key={change}>{change}</li>
                         ))}
