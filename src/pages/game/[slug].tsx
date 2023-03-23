6a3729503dcda217b97a0b7830b4a80e279af3dd
Lucas Winkler <lucaswinkler@gmail.com>
2023-03-22 20:24:26 -0400

    Adjusted information on games with no clip

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -29,15 +29,51 @@ const Game: FC<GameProps> = ({ game }) => {
 
   const ranks = game.ranks;
 
+  // The display and content will change later to let the user know there isn't a game today.
+  // It will most likely also suggest the user to submit their own clips to help.
   if (!game.currentClip) {
     return (
       <>
         <GameWrapper game={game}>
-          <h3 className='pt-8 pb-2 text-center text-4xl'>Work In Progress</h3>
-          <p className='pb-8 text-lg text-neutral-200'>
-            If youre seeing this page, it means the game is still in development
-            or there isn&apos;t a clip for today!
-          </p>
+          <div className='mx-auto max-w-2xl text-center text-lg text-neutral-200'>
+            <h3 className='pt-8 text-2xl text-neutral-100'>Work In Progress</h3>
+            <p className='pt-8'>
+              If youre seeing this page, it means the game is still in
+              development! This page will be eventually let you know that the
+              game currently does not have a clip today. It will also suggest
+              you to submit your own clips to help.
+            </p>
+            <div className='mx-auto max-w-xl pt-14'>
+              <h4 className='text-2xl text-neutral-100'>How you can help</h4>
+              <p className='pt-4'>
+                User submissions will be the last feature available on the
+                website. If you want to help get the game up and running sooner
+                please send your own clips to{' '}
+                <span>bliitzzedits@gmail.com</span>
+              </p>
+              <ul className='pt-4'>
+                <li>
+                  <span className='font-bold'>Clip length:</span> 0:30 - 2:00
+                </li>
+                <li>
+                  <span className='font-bold'>Clip quality:</span> 720p or
+                  higher
+                </li>
+                <li>
+                  <span className='font-bold'>Clip format:</span> Any for now
+                </li>
+                <li>
+                  <span className='font-bold'>Clip size:</span> Any for now
+                </li>
+                <li>No background music</li>
+                <li>You must blur/cover any rank indicators</li>
+              </ul>
+              <p className='pt-4'>
+                Once the game is up and running the rules will be a bit more
+                strict.
+              </p>
+            </div>
+          </div>
         </GameWrapper>
       </>
     );
