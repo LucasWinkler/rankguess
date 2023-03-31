baa4acd7bb896286ce69c88437266b183081c683
Lucas Winkler <lucaswinkler@gmail.com>
2023-03-31 18:10:27 -0400

    Added dynamic ticks to the health bar

--- src/components/game/HealthBar.tsx
+++ src/components/game/HealthBar.tsx
@@ -10,6 +10,23 @@ type HealthBarProps = {
 
 const HealthBar: FC<HealthBarProps> = ({ guessesLeft, className }) => {
   const health = (100 / MAX_GUESS_COUNT) * guessesLeft;
+
+  const healthTicks = Array.from({ length: MAX_GUESS_COUNT - 1 }, (_, i) => {
+    const tickPosition = (100 / MAX_GUESS_COUNT) * (i + 1);
+
+    // Ensures we don't draw a tick at the end of the bar
+    if (health <= tickPosition) {
+      return;
+    }
+
+    return (
+      <div
+        key={i}
+        className='absolute top-[50%] bottom-[50%] h-[90%] w-[1px] bg-blueish-grey-700/50 [transform:translate(-50%,-50%)]'
+        style={{ left: `${tickPosition}%` }}></div>
+    );
+  });
+
   return (
     <div
       className={clsx(
@@ -20,9 +37,12 @@ const HealthBar: FC<HealthBarProps> = ({ guessesLeft, className }) => {
         <Heart />
       </div>
       <div className='flex h-[2.5rem] w-full items-center justify-start rounded-[10px] border border-blueish-grey-600/50 bg-blueish-grey-600/50'>
-        <div
-          className='mx-2 h-[60%] w-full rounded-[5px] border border-green-400/50 bg-green-400/50'
-          style={{ width: `${health}%` }}></div>
+        <div className='relative mx-2 h-[60%] w-full overflow-hidden'>
+          {healthTicks}
+          <div
+            className='h-full w-full rounded-[5px] border border-green-400/50 bg-green-400/50'
+            style={{ width: `${health}%` }}></div>
+        </div>
       </div>
     </div>
   );
