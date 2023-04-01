ba027e2c74d9907d6458c6f603b4bf7f211bbdbd
Lucas Winkler <lucaswinkler@gmail.com>
2023-04-01 04:15:12 -0400

    Testing custom header

--- next.config.js
+++ next.config.js
@@ -35,6 +35,21 @@ const nextConfig = {
       },
     ],
   },
+  headers: async () => {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 };
 
 module.exports = withPlaiceholder(nextConfig);
